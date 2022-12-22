b7432a0923c4211c8812ef95be0d57c1dc63bcf8
kodarov_salavat <kodarov@gmail.com>
2022-12-22 14:08:40 +0400

    ДЗ3/Задача1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,57 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+        task6();
+        task7();
+        task8();
+    }
+    public static void task1()
+    {
+        System.out.println("Задача 1");
+        byte myByte = 100;
+        short myShort = 10000;
+        int myInt = 100000000;
+        long myLong = 1000000000000000L;
+        float myFloat = 0.00001f;
+        double myDouble = 0.100000000000000000000000001;
+        System.out.println("Значение переменной myByte с типом byte равно " + myByte);
+        System.out.println("Значение переменной myShort с типом short равно " + myShort);
+        System.out.println("Значение переменной myInt с типом int равно " + myInt);
+        System.out.println("Значение переменной myLong с типом long равно " + myLong);
+        System.out.println("Значение переменной myFloat с типом float равно " + myFloat);
+        System.out.println("Значение переменной myDouble с типом double равно " + myDouble);
+    }
+    public static void task2()
+    {
+        System.out.println("Задача 2");
+    }
+    public static void task3()
+    {
+        System.out.println("Задача 3");
+    }
+    public static void task4()
+    {
+        System.out.println("Задача 4");
+    }
+    public static void task5()
+    {
+        System.out.println("Задача 5");
+    }
+    public static void task6()
+    {
+        System.out.println("Задача 6");
+    }
+    public static void task7()
+    {
+        System.out.println("Задача 7");
+    }
+    public static void task8()
+    {
+        System.out.println("Задача 8");
     }
 }
